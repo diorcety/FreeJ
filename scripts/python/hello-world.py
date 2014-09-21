7c625aab84c7886fcc26fa4a41ea9f808244d40d
Yann Diorcet <diorcet.yann@gmail.com>
2014-09-21 20:46:46 +0200

    Add python examples

---
+++ scripts/python/hello-world.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# System wide useful modules
+import threading
+import time
+import freej
+# initializes FreeJ creating a Context
+cx = freej.Context()
+# creates a screen (different to docs)
+scr = freej.SdlScreen()
+# Initialise the screen to the given size (I chose Widescreen PAL resolution) (not in docs)
+scr.init(1024,576,32)
+# adds the screen
+cx.add_screen(scr)
+# create an instance of a TextLayer
+txt = freej.TextLayer()
+# initializes the new layer with the freej context (different to docs - optionally takes x,y,bpp size)
+txt.init()
+# writes the hello world text inside the layer
+txt.write("Hello World!")
+# start the layer
+txt.start()
+# add the layer to the screen
+cx.add_layer(txt)
+# starts freej in a separate thread
+th = threading.Thread(target = cx.start , name = "freej")
+th.start()
---
+++ scripts/python/play-effect.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# System wide useful modules
+import threading
+import time
+import sys
+import freej
+# initializes FreeJ creating a Context
+cx = freej.Context()
+# creates a screen (different to docs)
+scr = freej.SdlScreen()
+# Initialise the screen to the given size (I chose Widescreen PAL resolution) (not in docs)
+scr.init(1024,576,32)
+# adds the screen
+cx.add_screen(scr)
+# refreshes the list of available filter effects
+cx.plugger.refresh(cx)
+# check that we have an argument
+if(sys.argv.__len__()<2):
+print "[!] this script needs an argument: file to play"
+quit()
+# opens the file given on commandline as a layer
+lay = cx.open(sys.argv[1])
+# gets the Water filter effect
+filt = cx.filters["Water"]
+# adds the filter to the layer
+lay.add_filter(filt)
+# start the layer thread
+lay.start()
+# adds the layer to the freej context
+cx.add_layer(lay)
+# starts freej in a separate thread
+th = threading.Thread(target = cx.start , name = "freej")
+th.start()
